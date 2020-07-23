7a53f67cf0a1f51aa2aaa3296fe4d3a7f9be3c5f
Leminkay <Leminkay@gmail.com>
2020-07-23 19:08:12 +0500

    auto-update of db + fixed get_spec_list query

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
-import requests
+import threading
 from datetime import datetime
+import time
 import telebot
 import dbstuff
 from parsetable import specValue
+
 
 def get_position(db, name, spec):
     specData = db.get_spec_list(spec)
@@ -11,26 +13,43 @@ def get_position(db, name, spec):
             return i + 1
 
 
+def loop_update():
+    while True:
+        print('updating db')
+        db = dbstuff.DbQuery()
+        c_time = int(datetime.today().timestamp())
+        db.insert_all(c_time)
+        del db
+        print('done updating')
+        time.sleep(7200)
 
-TOKEN = '1394904859:AAGnS2IQuE3EDOnwI69bCt6HovM-a3U5Luc'
+
+TOKEN = '1394904859:AAG--El2aQs5XPNnZl9LvD2gQaGpsFmXik4'
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start','help'])
+@bot.message_handler(commands=['start', 'help'])
 def help_message(message):
-    bot.reply_to(message,"Чтобы узнать свое место в списках поступающих напишите свои Фамилия Имя Отчество")
+    bot.reply_to(message, "Чтобы узнать свое место в списках поступающих напишите свои Фамилия Имя Отчество")
+
+
 @bot.message_handler(content_types=['text'])
 def get_name(message):
     db = dbstuff.DbQuery()
     result = db.find_user(message.text)
+    print(message.text)
     answer = ''
-    if len(result) == 0:
 
+    if len(result) == 0:
         answer = "Такой Абитуриент не найдет"
     else:
         for row in result:
-            answer += "\"" + str(specValue[row[9]]) + "\"" + ' место: ' + str(get_position(db, message.text, row[9])) + '\n'
+            answer += "\"" + str(specValue[row[9]]) + "\"" + ' место: ' + str(
+                get_position(db, message.text, row[9])) + '\n'
     bot.reply_to(message, answer)
-#9 for spec
+# 9 for spec
 
+
+x = threading.Thread(target=loop_update)
+x.start()
 bot.polling()
